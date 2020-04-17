147092c198ceaa0633f19c973fa56f11ada92bb3
Gustavo Larsen <55494775+gustavolarsen@users.noreply.github.com>
2020-04-17 13:08:32 -0300

    Ajuste para utilização de Async Await

--- test/server.test.js
+++ test/server.test.js
@@ -2,9 +2,7 @@ const supertest = require('supertest');
 
 const request = supertest('http://localhost:3000');
 
-test('Deve responder na porta 3000', () => {
-  return request.get('/')
-    .then( (res) => {
-      expect(res.status).toBe(200);
-    }); 
+test('Deve responder na porta 3000', async () => {  
+  const response = await request.get('/').send();
+  expect(response.status).toBe(200);
 }); 
